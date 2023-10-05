4b7f64c3f5dd74f175001f94523c5e45edeb0339
Gavin Reid <141417767+GavinCodesThings@users.noreply.github.com>
2023-10-05 08:48:11 -0400

    Update AutoblockA.java

--- src/main/java/ac/grim/grimac/checks/impl/combat/autoblock/AutoblockA.java
+++ src/main/java/ac/grim/grimac/checks/impl/combat/autoblock/AutoblockA.java
@@ -18,14 +18,20 @@ public class AutoblockA extends Check implements PacketCheck {
     Yes I know my code is messy
      */
 
+    // TODO: Make sure this works
+
     @Override
     public void onPacketReceive(PacketReceiveEvent event) {
         if (event.getPacketType() == PacketType.Play.Client.INTERACT_ENTITY) {
             WrapperPlayClientInteractEntity wrapper = new WrapperPlayClientInteractEntity(event);
-            if (wrapper.getAction() == WrapperPlayClientInteractEntity.InteractAction.ATTACK && event.getPlayer() instanceof Player) {
-                Player plr = (Player) event.getPlayer();
-                if (plr.isBlocking()) {
-                    flagAndAlert();
+            if (wrapper.getAction() == WrapperPlayClientInteractEntity.InteractAction.ATTACK) {
+                for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                    if (wrapper.getPacketEntityID() == player.bukkitPlayer) {
+                        Player plr = onlinePlayer;
+                    if (plr.isBlocking()) {
+                        flagAndAlert();
+                    }
+                    }
                 }
             }
         }
